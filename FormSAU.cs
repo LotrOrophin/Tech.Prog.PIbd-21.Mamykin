2aace3809fd019204283ae3c1304629037824d1a
LotrOrophin <mamykin.vladimir@list.ru>
2019-11-25 11:47:25 +0400

    Fix

---
+++ FormSAU.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1;
+using static WindowsFormsApp1.MilitaryVehicle;
+
+namespace ProjectLabe
+{
+    public partial class FormSAU : System.Windows.Forms.Form
+    {
+        private MilitaryVehicle sau;
+        public FormSAU()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            sau.DrawCar(gr);
+            pictureBox.Image = bmp;
+        }
+        private void buttonCreateMilitaryVehicle(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            sau = new MilitaryVehicle(rnd.Next(100, 300), rnd.Next(1000, 2000), Color.Blue,false);
+            sau.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBox.Width,
+           pictureBox.Height);
+            Draw();
+        }
+        private void buttonMove_Click(object sender, EventArgs e)
+        {
+            //получаем имя кнопки
+            string name = (sender as Button).Name;
+            switch (name)
+            {
+                case "buttonUP":
+                    sau.MoveTransport(Direction.Up);
+                    break;
+                case "buttonDOWN":
+                    sau.MoveTransport(Direction.Down);
+                    break;
+                case "buttonLEFT":
+                    sau.MoveTransport(Direction.Left);
+                    break;
+                case "buttonRIGHT":
+                    sau.MoveTransport(Direction.Right);
+                    break;
+            }
+            Draw();
+        }
+
+        private void buttonCreateSAU(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            sau = new SAU(rnd.Next(100, 300), rnd.Next(1000, 2000), Color.Blue, Color.Yellow, true, true);
+            sau.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBox.Width,
+           pictureBox.Height);
+            Draw();
+        }
+    }
+}
---
+++ SAU.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace WindowsFormsApp1
+{
+    class SAU : MilitaryVehicle
+    {
+        
+        public Color DopColor { private set; get; }
+
+        public bool DopKoleso { private set; get; }
+
+
+    
+
+        public SAU(int maxSpeed, float weight, Color mainColor, Color dopColor, bool dopKoleso, bool phara):
+            base (maxSpeed, weight, mainColor, phara)
+
+        {
+            DopColor = dopColor;
+            DopKoleso = dopKoleso;
+        }
+       
+        public override void MoveTransport(Direction direction)
+        {
+            float step = MaxSpeed * 100 / Weight;
+            switch (direction)
+            {
+                // вправо
+                case Direction.Right:
+                    if (_startPosX + step < _pictureWidth - sauWidth)
+                    {
+                        _startPosX += step;
+                    }
+                    break;
+                //влево
+                case Direction.Left:
+                    if (_startPosX - step > 0)
+                    {
+                        _startPosX -= step;
+                    }
+                    break;
+                //вверх
+                case Direction.Up:
+                    if (_startPosY - step - 90 > 0)
+                    {
+                        _startPosY -= step;
+                    }
+                    break;
+                //вниз
+                case Direction.Down:
+                    if (_startPosY + step < _pictureHeight - sauHeight)
+                    {
+                        _startPosY += step;
+                    }
+                    break;
+            }
+        }
+        public override void DrawCar(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black);
+            g.DrawLine(pen, _startPosX, _startPosY - 6, _startPosX - 5, _startPosY + 2);
+            g.DrawLine(pen, _startPosX + 90, _startPosY - 6, _startPosX + 95, _startPosY + 2);
+
+            Brush koleso = new SolidBrush(Color.Black);
+            g.FillEllipse(koleso, _startPosX + 2, _startPosY - 13, 20, 20);
+            if (DopKoleso)
+                g.FillEllipse(koleso, _startPosX + 24, _startPosY - 13, 20, 20);
+            g.FillEllipse(koleso, _startPosX + 67, _startPosY - 13, 20, 20);
+
+
+
+
+            Brush unit = new SolidBrush(DopColor);
+            g.FillRectangle(unit, _startPosX + 5, _startPosY - 25, 40, 19);
+           // g.FillEllipse(unit, _startPosX + 12, _startPosY - 100, 22, 20);
+
+            Brush korpus = new SolidBrush(MainColor);
+            g.FillRectangle(korpus, _startPosX + 45, _startPosY - 35, 25, 29);
+            g.FillRectangle(korpus, _startPosX + 70, _startPosY - 27, 20, 21);
+            g.FillRectangle(korpus, _startPosX + 10, _startPosY - 33, 28, 10);
+
+            Brush pushka = new SolidBrush(Color.Black);
+            //g.FillRectangle(pushka, _startPosX + 14, _startPosY - 90, 20, 65);
+            int i = 0;
+            while (i < 7) { 
+            g.DrawLine(pen, _startPosX + 14+i, _startPosY - 25, _startPosX + 40+i, _startPosY - 90);
+                i++;
+            }
+            i = 0;
+            while (i < 7)
+            {
+                g.DrawLine(pen, _startPosX + 26 + i, _startPosY - 25, _startPosX + 52 + i, _startPosY - 90);
+                i++;
+            }
+            
+            g.FillEllipse(koleso, _startPosX, _startPosY - 10, 90, 4);
+
+            g.DrawRectangle(pen, _startPosX + 45, _startPosY - 35, 45, 29);
+
+             if (Phara)
+                 g.FillRectangle(unit, _startPosX + 80, _startPosY - 20, 10, 5);
+
+        }
+    }
+}
